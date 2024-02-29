ceadc823fba815ae761dad6b9df36a052336bcd4
Thomas Fahy <thomaspfahy@gmail.com>
2024-02-28 19:04:34 -0500

    Exercise

---
+++ WorkFlow.java
@@ -0,0 +1,19 @@
+public class WorkFlow {
+    private int wfid;
+    private String step; 
+    private int objid;
+    
+    
+    public Boolean addWFItem(String step, int objid) {
+        return true;
+    }
+
+    public int getNextWFItem(String step) {
+        // this.objid++;
+        return 3; 
+    }
+
+    public countWFItems(String step) {
+        return 3;     
+    }
+}
