14808a06a138452e512c2ab33a4047fbbd210829
Thomas Fahy <thomaspfahy@gmail.com>
2024-03-21 19:56:10 -0400

    Generic approval class

--- workflow/src/main/java/com/group16/Approval.java
+++ workflow/src/main/java/com/group16/Approval.java
@@ -1,2 +1,29 @@
-package com.group16;public class Approval {
+package com.group16;
+
+public class Approval {
+    private WorkFlow currentWF;
+
+
+
+    public WorkFlow getNextItem(){
+
+        return null;
+    }
+
+    public WorkFlow getCurrentWorkFlow(){
+        return this.currentWF;
+    }
+
+    public void setCurrentWF(WorkFlow wf){
+        this.currentWF = wf;
+    }
+
+    public boolean Review(WorkFlow wf){
+        return false;
+    }
+
+    public void sendToReviewer(WorkFlow wf){
+        // send to reviewer
+    }
+
 }
