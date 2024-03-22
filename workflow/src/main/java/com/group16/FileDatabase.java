81dfb15837e9c95e229ab552fe86b830c48607c7
Tushar <stuffcopresident@gmail.com>
2024-03-21 22:47:40 -0400

    added Database interface

---
+++ workflow/src/main/java/com/group16/FileDatabase.java
@@ -0,0 +1,45 @@
+package com.group16;
+
+import java.util.Set;
+import java.util.function.Predicate;
+
+public class FileDatabase implements Database {
+    private String filename;
+    public FileDatabase(String filename) {
+        this.filename = filename;
+    }
+    @Override
+    public void connect() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void disconnect() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void delete(int objId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void insert(WorkflowObject obj) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Set<WorkflowObject> select(Predicate<WorkflowObject> filter) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void update(WorkflowObject obj) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
