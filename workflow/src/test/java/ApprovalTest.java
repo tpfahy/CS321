e39405edd6baa3c13f9e3ebc1728f9af9d4a577e
Thomas Fahy <thomaspfahy@gmail.com>
2024-03-21 22:16:27 -0400

    Start of approval testing

---
+++ workflow/src/test/java/ApprovalTest.java
@@ -0,0 +1,35 @@
+import com.group16.Approval;
+import com.group16.WorkFlow;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class ApprovalTest {
+
+    @Test
+    void TestSetWF(){
+        WorkFlow wf = new WorkFlow();
+        Approval approver = new Approval();
+
+        approver.setCurrentWF(wf);
+        assertEquals(approver.getCurrentWorkFlow(), wf);
+    }
+
+    @Test
+    void TestReview(){
+        WorkFlow wf = new WorkFlow();
+        Approval approver = new Approval();
+        assertEquals(approver.review(wf), false);
+
+    }
+
+    @Test
+    void TestSendToReviewer(){
+        WorkFlow wf = new WorkFlow();
+        Approval approver = new Approval();
+        assertEquals(approver.sendToReviewer(wf), true);
+    }
+
+
+}
