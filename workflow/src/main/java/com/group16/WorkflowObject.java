2b04fc00586179fe19a4d9fa591ab962899d3224
Thomas Fahy <thomaspfahy@gmail.com>
2024-03-21 23:06:47 -0400

    Changes to the workflow object class

--- workflow/src/main/java/com/group16/WorkflowObject.java
+++ workflow/src/main/java/com/group16/WorkflowObject.java
@@ -1,5 +1,31 @@
 package com.group16;
 
+
+import java.time.LocalDate;
+
 public class WorkflowObject {
 
+    private String objectID;
+    private GreenCardData data;
+    private LocalDate dateCreated;
+    private LocalDate dateModied;
+    private WorkflowStatus workflowStatus;
+
+
+
+    public String getObjectID(){
+        return this.objectID;
+    }
+
+    public GreenCardData getFormData(){
+        return this.data;
+    }
+
+    public WorkflowStatus getWorkflowStatus(){
+        return this.workflowStatus;
+    }
+
+    public void setWorkflowStatus(WorkflowStatus workflowStatus) {
+        this.workflowStatus = workflowStatus;
+    }
 }
